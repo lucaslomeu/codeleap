2019d1cb00c39d25708c4a281f8674f17fe0e693
Lucas Lomeu <lucaslomeugomes@gmail.com>
2025-05-19 18:22:49 -0300

    [FIX] Settings and Requirements

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from firebase_admin import credentials
 import os
 import json
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,12 +82,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
