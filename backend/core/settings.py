70147896549b82d747fbeca568d93eb01ad8c33c
Lucas Lomeu <lucaslomeugomes@gmail.com>
2025-05-19 19:09:41 -0300

    [FIX] Settings and Requirements

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -48,8 +48,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,12 +126,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# CORS_ALLOWED_ORIGINS = [
-#     "https://codeleap-rho.vercel.app",
-#     "https://codeleap-git-master-lucaslomeus-projects.vercel.app",
-# ]
-
-CORS_ALLOWED_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://codeleap-lucaslomeus-projects.vercel.app",
+    "https://codeleap-git-master-lucaslomeus-projects.vercel.app",
+    "https://codeleap-rho.vercel.app",
+]
 
 firebase_json = os.environ.get('FIREBASE_SERVICE_ACCOUNT_JSON')
 
